86ef236a94582e26ce71cb1a5f49796bdc40f171
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-09-04 01:31:37 -0400

    install djangorestframework and create the beginnings of the client-side models

---
+++ polls_api/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+
+
+urlpatterns = patterns('polls_api.views',
+    url(r'^restframework', include('djangorestframework.urls', namespace='djangorestframework')),
+    url(r'^(?P<poll_id>\d+)/$', 'poll_results_view', name='polls_api_results'),
+)
