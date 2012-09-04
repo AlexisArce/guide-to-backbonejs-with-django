040e5c81b159739eb25c4554da10e4995db54da0
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-09-04 02:51:02 -0400

    move the serialization of results into a resource

---
+++ polls_api/resources.py
@@ -0,0 +1,14 @@
+from djangorestframework import resources
+from polls.models import Poll
+
+class PollResource (resources.ModelResource):
+    model = Poll
+    fields = ('question', 'choices')
+    exclude = ()
+
+    def choices(self, poll):
+        return [{
+                    'id': choice.id,
+                    'choice': choice.choice,
+                    'votes': choice.votes
+                } for choice in poll.choice_set.all()]
--- polls_api/views.py
+++ polls_api/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import get_object_or_404
 from djangorestframework import views
 from polls.models import Poll
+from .resources import PollResource
 
 class PollResults (views.View):
 
     def get(self, request, poll_id):
         poll = get_object_or_404(Poll.objects.all(), pk=poll_id)
-        results = {
-            'question': poll.question,
-            'choices': [{
-                'id': choice.id,
-                'choice': choice.choice,
-                'votes': choice.votes
-            } for choice in poll.choice_set.all()]
-        }
+        results = PollResource().serialize(poll)
         return results
 
 poll_results_view = PollResults.as_view()
